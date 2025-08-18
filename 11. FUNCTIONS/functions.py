a5ad8f71a2515a4f3ba9315a74760857f25472cb
sim-17-afk <sawantsimoni@gmail.com>
2025-08-18 14:18:20 +0530

    Add lambda functions program

---
+++ 11. FUNCTIONS/functions.py
@@ -0,0 +1,13 @@
+def greet(name):
+    print(f"Hello, {name}!")
+
+def add(a, b):
+    return a + b
+
+def main():
+    greet("Simoni")
+    result = add(5, 3)
+    print("Sum of 5 and 3 is:", result)
+
+if __name__ == "__main__":
+    main()
---
+++ 12. LAMBDA FUNCTIONS/lambda_functions.py
@@ -0,0 +1,8 @@
+# Lambda function to add two numbers
+add = lambda a, b: a + b
+
+# Lambda function to square a number
+square = lambda x: x * x
+
+print("Sum of 4 and 5 is:", add(4, 5))
+print("Square of 6 is:", square(6))
